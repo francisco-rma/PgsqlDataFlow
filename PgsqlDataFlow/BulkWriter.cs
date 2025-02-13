c76620dded836a91c4cf13da77f80a934208c014
Francisco Andrade <frma059@gmail.com>
2025-02-13 17:14:35 -0300

    Add project files.

---
+++ PgsqlDataFlow/BulkWriter.cs
@@ -0,0 +1,14 @@
+﻿using Npgsql;
+
+namespace PgsqlDataFlow
+{
+    public class BulkWriter
+    {
+        public NpgsqlDataSource dataSource { get; set; }
+        public BulkWriter(string connectionString)
+        {
+            dataSource = NpgsqlDataSource.Create(connectionString);
+        }
+
+    }
+}
